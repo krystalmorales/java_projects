dda6d7f5500f8c0862cb17407d5791f166c0ded8
krystalmorales <109539024+krystalmorales@users.noreply.github.com>
2023-10-16 17:06:38 -0600

    Create exercise4.java

---
+++ week1/methods-exercises/src/exercise4.java
@@ -0,0 +1,23 @@
+public class Exercise04 {
+
+    public static void main(String[] args) {
+        System.out.println(getFirstVowel("magnificent")); // Expected: a
+        System.out.println(getFirstVowel("winsome")); // Expected: i
+        System.out.println(getFirstVowel("xxx")); // Expected:
+
+        // 2. Call getFirstVowel at least one more time.
+    }
+
+    // getFirstVowel returns the first vowel in a string as a char.
+    // 1. Complete getFirstVowel.
+    // If no vowel is found, return 0. (As a char, 0 represents the NULL value.)
+    public static char getFirstVowel(String value) {
+        for (int i = 0; i < value.length(); i++) {
+            char ch = Character.toLowerCase(value.charAt(i));
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return value.charAt(i);
+            }
+        }
+        return '0';
+    }
+}
